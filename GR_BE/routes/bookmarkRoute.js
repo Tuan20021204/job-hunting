a32cfee7ef61178ad9292be0225f691a7c98212b
NHMHieuA1K57 <124542355+NHMHieuA1K57@users.noreply.github.com>
2025-03-06 10:06:22 +0700

    Revert "Merge pull request #12 from Tuan20021204/Tuan"
    This reverts commit 910b956e91ebadc9a9a6a39b7c77afe6fde8450a, reversing
    changes made to c1cc87afa935c7d7e878e0d2b691ec268929a3a7.

--- GR_BE/routes/bookmarkRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const companyController = require("../controllers/bookmarkController");
-const router = express.Router();
-
-router.delete("/:id", companyController.deleteBookmark);
-
-module.exports = router;
